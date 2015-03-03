e11c8a0c402b11e3f39eb475abbfc30eccbc8975
gaelach <brendan.gaelach@gmail.com>
2015-03-03 11:11:05 -0800

    updated #include

--- Source/EnemyEntity.cpp
+++ Source/EnemyEntity.cpp
@@ -6,4 +6,28 @@
 //
 //
 
-#include <stdio.h>
+#include <iostream>
+
+#include "EnemyEntity.hpp"
+
+EnemyEntity::EnemyEntity ( Map & map )
+{
+
+}
+
+EnemyEntity::EnemyEntity ( Map & map , const sf::Vector2f & position , const sf::Texture & texture ,
+             unsigned int healthPoints , unsigned int maximumHealth , float detectionDistance )
+{
+
+}
+EnemyEntity::~EnemyEntity () = default;
+
+/* this is a distance in pixels in which the artifical intelligence of the enemies
+ recognizes the player and attacks them */
+void EnemyEntity::setDetectionDistance ( float detectionDistance ) ;
+float EnemyEntity::getDetectionDistance ( ) const ;
+
+/*  */
+void EnemyEntity::update ( const sf::Time & frameTime ) ;
+/*  */
+void EnemyEntity::draw ( sf::RenderTarget & target ) const ;
