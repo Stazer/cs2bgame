7ab15593fabe7789194039b89296295469100de2
Justus <justusflerlage@taista.net>
2015-03-04 23:18:34 -0800

    implemented correct texturing for map areas

--- Source/MapArea.cpp
+++ Source/MapArea.cpp
@@ -1,12 +1,48 @@
 #include "MapArea.hpp"
+#include "Map.hpp"
+#include "Game.hpp"
 
-MapArea::MapArea ( MapArea::TileType type , const sf::Vector2f & position , const sf::Vector2f & size ) :
+MapArea::MapArea ( Map & map , MapArea::TileType type , const sf::Vector2f & position , const sf::Vector2f & size ) :
+	map ( map ) ,
 	type ( type ) ,
 	position ( position ) ,
 	size ( size )
 {
+    sf::Texture * texture = nullptr ;
+
+    if ( type == MapArea::Dirt )
+        texture = & map.getGame ( ).getTextureManager ( ).get ( "Dirt" ) ;
+    else
+        texture = & map.getGame ( ).getTextureManager ( ).get ( "Grass" ) ;
+
+    const float maxPositionX = position.x + size.x ;
+    const float maxPositionY = position.y + size.y ;
+
+    for ( float x = position.x ; x < maxPositionX ; x += texture->getSize ( ).x )
+    {
+        for ( float y = position.y ; y < maxPositionY ; y += texture->getSize ( ).y )
+        {
+            sf::Sprite sprite ( * texture ) ;
+
+            sprite.setPosition ( sf::Vector2f ( x , y ) ) ;
+
+            const float tempX = x + texture->getSize().x ;
+            const float tempY = y + texture->getSize().y;
+
+            sf::Vector2i newSize (texture->getSize ( ).x , texture->getSize().y);
+
+            if (tempX > maxPositionX)
+                newSize.x -= tempX - maxPositionX ;
+            if (tempY > maxPositionY)
+                newSize.y -= tempY - maxPositionY ;
+
+            sprite.setTextureRect(sf::IntRect (sf::Vector2i (),newSize));
+
+            this->tiles.push_back ( sprite ) ;
+        }
+    }
 }
-		
+
 MapArea::TileType MapArea::getType ( ) const
 {
 	return this->type ;
--- Source/MapArea.hpp
+++ Source/MapArea.hpp
@@ -8,6 +8,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+class Map ;
+
 class MapArea
 {
 	public :
@@ -18,19 +20,21 @@ class MapArea
 			Dirt ,
          	Lava
 		} ;
-		
-		MapArea ( TileType type , const sf::Vector2f & position , const sf::Vector2f & size ) ;
-		
+
+		MapArea ( Map & map , TileType type , const sf::Vector2f & position , const sf::Vector2f & size ) ;
+
 		TileType getType ( ) const ;
 		const sf::Vector2f & getPosition ( ) const ;
 		const sf::Vector2f & getSize ( ) const ;
 
       /*  */
 		void draw ( sf::RenderTarget & target ) const ;
-	
+
 	private :
+        Map & map ;
+
 		TileType type ;
-	
+
 		sf::Vector2f position ;
 		sf::Vector2f size ;
 
