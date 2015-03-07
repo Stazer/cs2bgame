6f7a8163774abab39b33acbfeb1a566ec2d15bba
Justus <justusflerlage@taista.net>
2015-03-07 11:53:24 -0800

    added definitions of StaticEntity

--- Source/StaticEntity.cpp
+++ Source/StaticEntity.cpp
@@ -1,9 +1,14 @@
-//
-//  StaticEntity.cpp
-//  cs2bgame
-//
-//  Created by Brendan Murray on 3/2/15.
-//
-//
+#include "StaticEntity.hpp"
 
-#include <stdio.h>
+StaticEntity::StaticEntity ( Map & map ) :
+	Entity ( map )
+{
+}
+StaticEntity::StaticEntity ( Map & map , const sf::Vector2f & position ,  const sf::Texture & texture ) :
+	Entity ( map , position , texture )
+{
+}
+		  
+void StaticEntity::update ( const sf::Time & frameTime )
+{
+}
