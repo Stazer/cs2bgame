dcd470212666efce6b63a62125d0a3ea37f713e7
Justus <justusflerlage@taista.net>
2015-03-09 15:43:30 -0700

    fixed compile issues

--- Source/StaticEntity.cpp
+++ Source/StaticEntity.cpp
@@ -6,14 +6,6 @@ StaticEntity::StaticEntity ( Map & map ) :
 }
 StaticEntity::StaticEntity ( Map & map , const sf::Vector2f & position ,  const sf::Texture & texture ) :
 	Entity ( map , position , texture )
-=======
-#include <stdio.h>
-
-StaticEntity::StaticEntity ( Map & map ) : Entity (map) { }
-
-StaticEntity::StaticEntity ( Map & map , const sf::Vector2f & position ,
-                            const sf::Texture & texture ) : Entity(map)
->>>>>>> 2de92c7999cc4e7c03d27524e240a8c84a157cf1
 {
 
 }
